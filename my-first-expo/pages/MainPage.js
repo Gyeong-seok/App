b553800fd51036349b40310df03d37120ea3dc8d
gyeong <gin3724@naver.com>
2022-08-12 01:17:20 +0900

    first commit

---
+++ my-first-expo/pages/MainPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import Back_Ground from '../assets/background1.jpg'
+import Main_Logo from '../assets/logo2.png'
+
+
+export default function App() {
+  return (
+      <ImageBackground source={require("../assets/background1.jpg")} style={styles.bgImage}>
+          <Image style ={styles.Logo} source = {require("../assets/Main_Logo.png")}
+          />
+        <ScrollView style={styles.back}>  
+        </ScrollView>
+      </ImageBackground> 
+  );
+}
+
+const styles = StyleSheet.create({
+  back: {
+    flex: 1,
+  },
+
+  bgImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode : 'stretch',
+  },
+
+  Logo : {
+    flexDirection : 'row',
+    justifyContent : 'center',
+
+  }
+});
