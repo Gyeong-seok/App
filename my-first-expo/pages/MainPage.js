bc5f23686c3b523cd3513ccffdbaf472270a01a8
gyeong <gin3724@naver.com>
2022-08-12 01:34:26 +0900

    first commit

--- my-first-expo/pages/MainPage.js
+++ my-first-expo/pages/MainPage.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import Back_Ground from '../assets/background1.jpg'
-import Main_Logo from '../assets/logo2.png'
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 
+
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import Back_Ground from "../assets/background1.jpg";
+import Main_Logo from "../assets/logo2.png";
 
 export default function App() {
   return (
-      <ImageBackground source={require("../assets/background1.jpg")} style={styles.bgImage}>
-          <Image style ={styles.Logo} source = {require("../assets/Main_Logo.png")}
-          />
-        <ScrollView style={styles.back}>  
-        </ScrollView>
-      </ImageBackground> 
+    <ImageBackground
+      source={require("../assets/background1.jpg")}
+      style={styles.bgImage}
+    >
+      <Image style={styles.Logo} source={require("../assets/Main_Logo.png")} />
+      <ScrollView style={styles.back}></ScrollView>
+    </ImageBackground>
   );
 }
 
@@ -24,12 +34,11 @@ const styles = StyleSheet.create({
   bgImage: {
     width: "100%",
     height: "100%",
-    resizeMode : 'stretch',
+    resizeMode: "stretch",
   },
 
-  Logo : {
-    flexDirection : 'row',
-    justifyContent : 'center',
-
-  }
+  Logo: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
 });
